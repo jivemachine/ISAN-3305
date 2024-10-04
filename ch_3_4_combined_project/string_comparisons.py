767723e16c3c5207d30adf9aff241a24fd205848
Jeremy <the.jeremy.cobb@gmail.com>
2024-10-04 15:42:28 -0500

    removed unneccessary files from project

--- ch_3_4_combined_project/string_comparisons.py
+++
@@ -1,7 +0,0 @@
-def greet_participant(name):
-    if name in keynote_speakers:
-        print(f"Hello {name}! We're excited for your presentation this evening.")
-    elif name in chairpersons:
-        print(f"Hello Chairperson {name}, we're honored to seat you this evening!")
-    else:
-        print(f"Hello {name}, welcome to the event!")
