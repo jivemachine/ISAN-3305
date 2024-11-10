fefe8c23f7ee1d9fb0aa1cf7aec3d99bbc02c233
Jeremy <the.jeremy.cobb@gmail.com>
2024-11-10 16:45:46 -0600

    completed step 2 displaying the grades from grades.txt and included some error handling incase file does not exist or is empty.

--- grading_project/main.py
+++ grading_project/main.py
@@ -38,10 +38,30 @@ def init_file_with_random_grades():
     student_list = generate_students()
     overwrite_txt_file(student_list)
             
-
+# function reads the grades.txt file and displays the students names
+# as well as their grades from the file
+def display_grades_from_file():
+    # file name contains the students and their grades
+    file = "grades.txt"
+    # check if file exists
+    try:
+        with open(file, "r") as f:
+            # if file exists check if it is empty
+            if f.read() == "":
+                print("No grades found.") # if empty print error message
+                return # return to main function
+            else:
+                # display the students and their grades from file
+                f.seek(0) # move read 'cursor' to the beginning of file
+                print(f.read()) # read and print the contents of the file to terminal      
+    # if file does not exists print error message
+    except FileNotFoundError:
+        print("No grades found.")
+        return # return to main function
 
 def main():
     init_file_with_random_grades()
+    display_grades_from_file()
     
     
     
