a8685ce64488e4e2e021579e81092cac7b9823b5
Jeremy <the.jeremy.cobb@gmail.com>
2024-11-10 17:31:25 -0600

    added functionality to update grades for existing students in grades.txt

--- grading_project/main.py
+++ grading_project/main.py
@@ -15,7 +15,7 @@ def generate_grade():
 # function generates a list of 8 students and randomizes their grades
 def generate_students():
     # List of 8 students
-    students = ["Alice", "Bob", "Charles", "James", "Dorothy", "Megan", "Oliver", "Samantha"]
+    students = ["Zhang", "Alice", "Bob", "Charles", "James", "Dorothy", "Oliver", "Samantha"]
     # List comprehensio to generate a list of 8 random grades
     grades = [generate_grade() for i in range(8)]
     # Zip the two lists together
@@ -105,11 +105,93 @@ def append_new_grades_to_file():
     # if student and grade are not None append student and grade to file
     if student and grade:
         append_student_grades(student, grade)
-    # else:
+        return # return to main function
+    else:
         print("Student and grade not added. Invalid input. Please try again.")
         return # return to main function
     
+# STEP 4
+# function reads grades.txt and returns a list of students and their grades
+def read_grades_from_file():   
+    # file name that contains students and grade data
+    file = "grades.txt"
+    # read the file and store contents in a list
+    with open(file, "r") as f:
+        # read the contents of the file
+        data = f.readlines()
+        # create an empty list to store the students and their grades
+        student_grades = []
+        # go through data and split the student name and grade
+        for line in data:
+            # split the line into student name and grade
+            name, grade = line.split(":")
+            # append the student name and grade to the list
+            student_grades.append((name.strip(), int(grade.strip())))
+        # return the list of students and their grades
+        return student_grades
+   
+# function inputs a list of students and their grades and sorts 
+# them by the name alphabetically as a numbered menu   
+def display_sorted_grades(student_grades):
+    # sort the students by name alphabetically
+    student_grades.sort()
+    # create a menu for the students
+    for i, student in enumerate(student_grades, 1):
+        print(f"{i}. {student[0]} - {student[1]}")
     
+# get student to update from user input
+def get_student_to_update(index):
+    # get user input for student name
+    student = input("Enter number for student to update: ")
+    # sanitize string input by removing leading and trailing white spaces
+    student = student.strip()
+    # validate if student is a number
+    try:
+        student = int(student)
+    except ValueError:
+        print("Student number is invalid.")
+        return None # if student is not valid return None to update_student_grade function
+    
+    # validate if student number is within the range of the students
+    if student < 1 or student > index:
+        print("Student selected tom update is invlaid.")
+        return None
+    return student # return student number to update_student
+
+# get new grade from user input
+def get_new_grade():
+    # get user grade for student
+    student_grade = input("Enter new student grade: ")
+    # sanitize integer input by removing leading and trailing white spaces
+    student_grade = student_grade.strip()
+    # validate if student grade is a number
+    try:
+        student_grade = int(student_grade)
+    except ValueError:
+        print("Grade must be a number.")
+        return None # if grade is not valid return None to update_student_grade function
+    
+    return student_grade # return student grade to update_student_grade
+
+# function updates the grade of a student in the grades.txt file
+def update_student_grade():
+    student_grades = read_grades_from_file()
+    # display the students and their grades
+    display_sorted_grades(student_grades)
+    # get the student to update
+    student = get_student_to_update(len(student_grades))
+    # if student is not valid return to main function
+    if student is None:
+        return
+    # get the new grade for the student
+    grade = get_new_grade()
+    # if grade is not valid return to main function
+    if grade is None:
+        return
+    # update the grade of the student
+    student_grades[student - 1] = (student_grades[student - 1][0], grade)
+    # overwrite the grades.txt file with the updated student grade
+    overwrite_txt_file(student_grades)
 
 def main():
     # initialize project by generating students and their grades
@@ -118,6 +200,10 @@ def main():
     display_grades_from_file()
     
     append_new_grades_to_file()
+    update_student_grade()
+    
+    
+    
     
     
     
