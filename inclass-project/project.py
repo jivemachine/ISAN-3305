91887a91dc43a1a68c4d2e8b1713e5de12f7b70f
Jeremy <the.jeremy.cobb@gmail.com>
2024-12-03 13:48:28 -0600

    refactor load_file function to enhance user interaction and error handling; implement file loading with user choice and log errors to CSV

--- inclass-project/project.py
+++ inclass-project/project.py
@@ -1,6 +1,8 @@
 import os
 import random as r
 from datetime import datetime
+import time
+import sys
 
 
 def load_file():
@@ -11,7 +13,7 @@ def load_file():
     if len(files) == 0:
         # exit function if no files are present
         print("No files found in 0_input directory.")
-        log_error(f"{generate_timestamp()} - No files found in 0_input directory.")
+        log_error(f"{generate_timestamp()},No files found in 0_input directory")
         return None
     
     # display files
@@ -27,45 +29,92 @@ def load_file():
             print(f"{file_count}. {file}")
         else:
             # log error message that file is not a csv file with timestamp and file name
-            log_error(f"{generate_timestamp()} - {file} is not a csv file.")
+            log_error(f"{generate_timestamp()},{file} is not a csv file")
         
     # if 0 csv files are found exit function
     if file_count == 0:
         print("No csv files found in 0_input directory.")
-        log_error(f"{generate_timestamp()} - No csv files found in 0_input directory.")
+        log_error(f"{generate_timestamp()},No csv files found in 0_input directory")
         return None
+    # add option add end for user to quit program
+    print(f"{file_count + 1}. Back to Main Menu.")
+
+    # Get user choice and handle errors
+    while True:
+        # get user input for file choice
+        user_choice = input("Enter the number associated with the file you would like to load: ")
+        user_choice.strip() # strip whitepace
+
+        # check if user input is a string
+        if user_choice.isalpha():
+            # error message if user choice is not an integer
+            print("Invalid choice. Please try again.")
+            log_error(f"{generate_timestamp()},User input is not an integer")
+        elif user_choice.isnumeric(): # checking is input is an integer
+            user_choice = int(user_choice)
+            # check if user wants to quit first
+            if user_choice == file_count + 1:
+                print("Back to main menu.")
+                return None
+            elif user_choice < 1 or user_choice > file_count:
+                # display error message if user choice is not within range
+                print("Invalid choice. Please try again.")
+                log_error(f"{generate_timestamp()},User input is out of range")
+                continue
+            else: # load file if user choice is valid
+                # get file name
+                file = files[user_choice - 1]
+                message = f"{file} loading..."
+                print_slow(message)
+            
+            try: # get data from file
+                file = "0_input/" + file
+                with open(file, "r") as f:
+                    # get data assume there are no headers in dataset
+                    data = f.readlines()
+                    return data
+            except:
+                log_error(f"{generate_timestamp()},Issue loading file - {file}")
+                print("Failed")
+                # return None
+        else: # for edge cases
+            return None 
     
-   
 
-
-    # try:
-    #     if os.path.exists(file):
-    #         with open(file, "r") as f:
-    #             # get headers
-    #             # headers = f.readline().strip().split(",")
-    #             # get data
-    #             data = f.readlines()
-    #             # convert data to list of dictionaries
-    #             # data = [dict(zip(row.strip().split(","))) for row in data]
-    #         return data
-    # except:
-    #     print("Failed")
-
+# write error message to /logs/errors_on_input.csv
 def log_error(message):
-    # log error message to error.txt file in logs folder
-    
     # check if logs folder exists within directory
-    if os.path.exists('logs'):
-        with open("logs/errors.txt", "a") as f:
-            f.write(f"{message}\n")
-    else: # create logs folder if it does not exist
-        os.mkdir("logs")
-        with open("logs/errors.txt", "a") as f:
-            f.write(f"{message}\n")
+    if not os.path.exists('logs'):
+        os.mkdir("logs") # create logs folder
+        with open("logs/errors_on_input.csv", "w") as f: # create csv file
+            f.write('id,timestamp,error_message\n') # write headers
+            f.write(f"0,{message}\n") # write error message
+    else:
+        # check if file exists
+        if os.path.exists("logs/errors_on_input.csv"):
+            # get current error log id
+            with open("logs/errors_on_input.csv", "r") as f:
+                id = len(f.readlines())
+            # write error message to file
+            if id >= 0:
+                with open("logs/errors_on_input.csv", "a") as f:
+                    f.write(f"{id},{message}\n")
+        else: # create file if it does not exist
+            with open("logs/errors_on_input.csv", "w") as f:
+                f.write('id,timestamp,error_message\n')
+                f.write(f"0,{message}\n")
+       
 
 # generates timestamp for error messages
 def generate_timestamp():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# prints text slowly
+def print_slow(input):
+    for char in input:
+        print(char, end='', flush=True)
+        time.sleep(0.1)
+    print() # move to next line
 
 def has_header():
     user_input = input("Does the first row contain header? y/n: ")
@@ -117,16 +166,24 @@ def display_menu():
     print("8. Check Values")
     print("9. Exit")
 
-
+# function handles displaying menu and user input
+def handle_menu():
+    data = None
+    has_header = False
+    while True:
+        display_menu()
+        choice = input("Enter choice: ")
+        choice.strip()
+        if choice.isnumeric():
+            choice = int(choice)
+            handle_menu_choice(choice, data, has_header)
+        else:
+            print("Invalid choice. Please try again.")
+            continue
+        
 
 def main():
-    # while True:
-        load_file()
-        # display_menu()
-        # choice = int(input("Enter your choice: "))
-        # data = load_file()
-        # print(data)
-        # has_header = handle_menu_choice(choice, data, has_header)
+    handle_menu()
 
 
 if __name__ == "__main__":
